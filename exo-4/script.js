19476cee835c5e49ba495fcb727706cc0648eb67
Annox <63057879+DylanBrss@users.noreply.github.com>
2022-06-15 14:42:24 +0200

    exo-4 fini

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,19 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+secretMessage.pop(); //supprimer la dernière valeur su tableau
+
+secretMessage.push('to','program'); //ajouter les mots a la fin du tableau
+
+secretMessage.indexOf('easily'); //rechercher l'index de easily
+
+secretMessage.splice(6,1,"right"); //change a l'index 6 le mots par right
+
+secretMessage.shift(); //suppression 1er élément du tableau
+
+secretMessage.unshift("Programming") // ajouter la chaine "Programming" en début de tableau
+
+secretMessage.splice(5,5,'know'); //change de 5 valeur a partir de l'index 5 par le mots par 'know'
+
+console.log(secretMessage.join(' ')); //afficher le message secret avec des espaces
