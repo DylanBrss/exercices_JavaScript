675d90aac3c395c041a9e1da7dc9154ebf7f3cd9
Annox <63057879+DylanBrss@users.noreply.github.com>
2022-06-15 15:24:30 +0200

    exo-5 fini

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,18 @@
 // you can write js here
-
 console.log('exo-5');
+
+var input = "Soixante rebelles s'incrustent sous terre.";
+var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+var resultArray = [];
+var i = 0;
+
+for (i = 0; i < input.length; i++) {
+    //console.log(input);
+    vowels.indexOf(input[i]);
+
+    if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'y') {
+        resultArray.push(input[i]);
+    }
+}
+
+console.log(resultArray.join('').toUpperCase());
