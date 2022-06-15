fa1530e1186161d0f205eb465d0571761cadefd6
Annox <63057879+DylanBrss@users.noreply.github.com>
2022-06-15 16:55:42 +0200

    exo-6 en cour

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -11,7 +11,48 @@ var joeInfo = {
 };
 
 // afficher le nombre de voitures de Joe
-
+joeInfo.cars.length;
 // changer le nombre de salle de bains de Joe : il n'en possède au'une.
-
+joeInfo.bathrooms = 1;
 // Joe vient d'acquérir un garage changer la structure pour le refléter.
+joeInfo.garage = true;
+
+var team = {
+    _player: {
+        firstName: "Pablo",
+        lastName: "Sanchez",
+        age: 11,
+    },
+    _games: {
+        opponent: "Broncos",
+        teamPoints: 42,
+        opponentPoints: 27,
+    },
+};
+
+function addplayer(first, last, age) {
+
+    for (let i = 0; i < team._player.length; i++) {
+        team._player.firstName = first;
+        team._player.lastName = last;
+        team._player.age = age;
+    }
+}
+addplayer("Lionel", "Messi", 36);
+addplayer("Marco", "Reus", 34);
+addplayer("Karim", "Benzema", 30);
+addplayer("Kinsley", "Coman", 26);
+
+
+function addmatch(opponent, teamPoints, opponentPoints) {
+    this.opponent = opponent;
+    this.teamPoints = teamPoints;
+    this.opponentPoints = opponentPoints;
+}
+team._games = new addmatch('Real Madrid', 35, 21);
+team._games = new addmatch('FC Barcelone', 42, 14);
+team._games = new addmatch('Borussia Dortmund', 51, 32);
+team._games = new addmatch('Fc Bayern Munich', 28, 10);
+
+
+console.log(team._player)
