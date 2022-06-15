c05992f339f2dfb0a0319513c75a123ebd9d93cd
Annox <63057879+DylanBrss@users.noreply.github.com>
2022-06-15 12:01:32 +0200

    exo-2 fini

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,22 @@
 // you can write js here
 console.log('exo-2');
+
+var InscriptionCourse = true; // variable Boolean
+var ageCoureur = 10;
+var raceNumber = Math.floor(Math.random()*1000);
+
+if(InscriptionCourse = true){
+    raceNumber;
+};
+
+
+
+if (ageCoureur >= 18 && InscriptionCourse == true) {
+    console.log('You will race at 9:30 am');
+} else if (ageCoureur >= 18 || InscriptionCourse == true) {
+    console.log('You will race at 11:00 am with bib number ' + raceNumber);
+} else if (ageCoureur < 18 && InscriptionCourse == false) {
+    console.log('You will race at 12:30 pm with bib number ' + raceNumber);
+}else {
+    console.log('Go to see the registration desk')
+}
